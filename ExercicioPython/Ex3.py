ce1f6ddb8a28d6c2816d2e67df2aa33579fff546
pplace11 <pedro.place@hotmail.com>
2024-11-15 17:13:09 +0000

    Update Ex3.py

--- ExercicioPython/Ex3.py
+++ ExercicioPython/Ex3.py
@@ -1,4 +1,6 @@
 num1 = int(input('Digite o primeiro número: '))
 num2 = int(input('Digite o segundo número: '))
+
 soma = num1 + num2
+
 print(f'A soma do valor {num1} mais o {num2} é igual a {soma}.')
