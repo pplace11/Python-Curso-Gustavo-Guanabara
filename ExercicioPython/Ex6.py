6eb4e4f4b1639dadb8b008361a341a06b4b0f9d5
pplace11 <pedro.place@hotmail.com>
2024-11-15 17:12:59 +0000

    Create Ex6.py

---
+++ ExercicioPython/Ex6.py
@@ -0,0 +1,8 @@
+numero = int(input('Qual é o valor? '))
+
+d = numero*2
+t = numero*3
+r = pow(numero, (1/2))
+    #numero**(1/2))
+
+print(f'O dobro é: {d} \nO triplo é: {t}\nA raiz quadrada é: {r:.2f}')
