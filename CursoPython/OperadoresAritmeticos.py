b25be3725d55f8385a16a3d545fd3b5c24066d86
pplace11 <pedro.place@hotmail.com>
2024-11-15 17:12:37 +0000

    Create OperadoresAritmeticos.py

---
+++ CursoPython/OperadoresAritmeticos.py
@@ -0,0 +1,9 @@
+n1 = int(input('Diga um valor: '))
+n2 = int(input('Diga mais um valor: '))
+s = n1+n2
+m = n1*n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+print(f'A soma vale {s},\nO produto vale {m},\nA divisao vale {d},\nA divisao inteira vale {di},\nA potencia vale {e}')
+
